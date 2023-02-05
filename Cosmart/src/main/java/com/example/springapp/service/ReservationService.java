23e2d2e2def6c885492b4480e3a08bf287357e4e
aston tampubolon <astontampubolon51@gmail.com>
2023-02-05 17:03:44 +0800

    [Cosmart] fix bug and add log

--- Cosmart/src/main/java/com/example/springapp/service/ReservationService.java
+++ Cosmart/src/main/java/com/example/springapp/service/ReservationService.java
@@ -18,18 +18,22 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.UUID;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 @Service
+@Slf4j
 public class ReservationService {
     private final Map<String, Reservation> reservationMap = new HashMap<>();
+    private static final String BOOK_URL = "http://openlibrary.org/subjects/love.json";
 
     public List<Book> getBooks(String subject) throws Exception {
+        log.info("request {}", subject);
         StringBuilder response = new StringBuilder();
         JsonParser jsonParser = new JsonParser();
         List<Book> listBook = new ArrayList<>();
         try {
-            URL url = new URL("http://openlibrary.org/subjects/love.json");
+            URL url = new URL(BOOK_URL);
             HttpURLConnection conn = (HttpURLConnection) url.openConnection();
             conn.setRequestMethod("GET");
             BufferedReader in = new BufferedReader(new InputStreamReader(conn.getInputStream()));
@@ -45,7 +49,7 @@ public class ReservationService {
                     JsonObject bookElement = item.getAsJsonObject();
                     JsonArray subjects = bookElement.getAsJsonArray("subject");
                     for (JsonElement e : subjects) {
-                        if (e.toString().contains(subject)) {
+                        if (e.getAsString().equalsIgnoreCase(subject)) {
                             Book newBook = new Book()
                                 .setTitle(bookElement.get("title").getAsString())
                                 .setEditionNumber(bookElement.get("edition_count").getAsString());
@@ -67,12 +71,14 @@ public class ReservationService {
                 });
             }
         } catch (Exception ex) {
+            log.warn("failed to get list of book, reason {}", ex.getMessage());
             throw new Exception(ex.getMessage());
         }
         return listBook;
     }
 
     public Reservation reserveBook(Reservation request) throws Exception {
+        log.info("request {}", request);
         String reservationId = UUID.randomUUID().toString();
         Calendar cal = Calendar.getInstance();
         int year = cal.get(Calendar.YEAR);
@@ -81,6 +87,7 @@ public class ReservationService {
         cal.set(year, month, day + 1, 0, 0, 0);
         Timestamp minDate = new Timestamp(cal.getTime().getTime());
         if (request.getReservationDate().before(minDate)) {
+           log.warn("Min reservation date {}", cal.getTime());
            throw new Exception("reservation date at least 1 day after now");
         }
         request.setReservationId(reservationId);
